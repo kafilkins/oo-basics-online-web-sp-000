a0f849f724059e711618f81fc79b37bea2726068
Keith Filkins <kafilkins@me.com>
2021-01-02 23:01:33 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,14 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :title, :author, :page_count, :genre
 
   def initialize(title)
     @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,12 +3,15 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
   end
 
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
-
 
 end
